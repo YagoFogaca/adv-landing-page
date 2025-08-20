fb93446eb56ef967bbf76281b36c3db2ccb93894
Yago Fogaça <yagofogaca20@gmail.com>
2025-08-20 19:29:29 -0300

    feat: implementação do menu de navegação

--- src/components/header/index.header.jsx
+++ src/components/header/index.header.jsx
@@ -1,6 +1,5 @@
 import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
 
-// import logo from "../../assets/imgs/logo-banner.png";
 import "./index.header.css";
 import { ImageComponent } from "../images/index.images";
 
@@ -8,7 +7,12 @@ export const HeaderComponents = () => {
   return (
     <>
       <>
-        <Navbar key={"md"} expand={"md"} className="bg-body-tertiary mb-3">
+        <Navbar
+          key="md"
+          expand="md"
+          className="mb-3"
+          bg="secondary"
+          data-bs-theme="dark">
           <Container fluid>
             <Navbar.Brand href="#">
               <ImageComponent
@@ -16,11 +20,13 @@ export const HeaderComponents = () => {
                 alt={"Logo escritorio de advocacia"}
               />
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+            <Navbar.Toggle aria-controls="offcanvasNavbar-expand-md" />
             <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-md`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-md`}
-              placement="end">
+              id="offcanvasNavbar-expand-md"
+              aria-labelledby="offcanvasNavbarLabel-expand-md"
+              placement="end"
+              bg="secondary"
+              data-bs-theme="dark">
               <Offcanvas.Header closeButton></Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
